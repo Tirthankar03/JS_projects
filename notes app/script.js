eed55871a491b44afc1f939da4e28e65d07d57db
Tirthankar Nath <tirthankar.nath2@gmail.com>
2023-04-21 02:00:16 +0530

    add + delete functionality added everything is added when the add note button is clicked, so wrapped everything inside it issues faced: 1. we mistyped the delete class in innerhtml because of which the deletebtn was returning undefined value 2. we were adding note class in the innerHTML that was supposed to be added later, as a result of which malfunction of ui happened because of the extra note div

--- notes app/script.js
+++ notes app/script.js
@@ -1,15 +1,42 @@
-//what we are thinking off is :
+
+const addBtn = document.getElementById('add');
+
+
+
+addBtn.addEventListener('click', () => {
+    addNewNote();
+});
+
+function addNewNote(){
+    //creating a new div nameed note
+    const noteNew = document.createElement('div'); 
+    noteNew.classList.add('note'); //adding a class to note
+    
+    noteNew.innerHTML = `
+    <div class="tools">
+                            <button class="edit"><i class="fas fa-edit"></i></button>
+                            <button class="delete"><i class="fas fa-trash"></i></button>
+                        </div>
+                        <div class="main hidden"></div>
+                        <textarea></textarea>
+                    </div> `;
+
+
+                    document.body.appendChild(noteNew); //adding note to the document body
+    //what we are thinking off is :
 //main display = none
 //when we click on the editBtn, then I show the other one
 //text area can have an input event listener such that:
     //when we type something, it updates the main
 
-const notesEl = document.querySelector('.notes');
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
+// const notesEl = document.querySelector('.note');  not added in html directly so not needed       
+const editBtn = noteNew.querySelector('.edit');
+const deleteBtn = noteNew.querySelector('.delete');
 
-const main = notesEl.querySelector('.main');
-const textArea = notesEl.querySelector('textarea');
+const main = noteNew.querySelector('.main');
+const textArea = noteNew.querySelector('textarea');
+
+
 
 
 editBtn.addEventListener('click',() => {
@@ -18,7 +45,6 @@ editBtn.addEventListener('click',() => {
 
 });
 
-
 //here JS destructuring has been used
 //the { value } is basically a property that has the value of the input
 //we renamed "value" to "keyboardInput" and used in in marked()
@@ -26,5 +52,12 @@ textArea.addEventListener('input',(x) => {
     const { value: keyboardInput } = x.target; //x denotes the input from the user and we are just storing the input into our var 'value'
     main.innerHTML = marked(keyboardInput);
 });
+
+deleteBtn.addEventListener('click',() => {
+    noteNew.remove();
+});    
+
+
+}
 
 
