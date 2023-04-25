fd741a2c407333b0998e8a7e567b417f288bf2c7
Tirthankar Nath <tirthankar.nath2@gmail.com>
2023-04-25 07:47:18 +0530

    the LS is applied and now the progress is stored even if the page is refreshed the app is complete✅

--- todo app/script.js
+++ todo app/script.js
@@ -2,9 +2,15 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todos = document.getElementById('todos');
 
+// const todos = JSON.parse(localStorage.getItem("todos"));
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    addTodo();
+});
 
+function addTodo() {
     const userInput = input.value;
     
     if(userInput){
@@ -21,10 +27,25 @@ form.addEventListener('submit', (e) => {
             e.preventDefault();
             todoEl.remove();
         });
-    
+        
         input.value = '';
+        updateLS();
     }
-});
+}
 
+function updateLS() {
+    const todoEls = document.querySelectorAll("li");
+    
+    const todoArr = [];
+    
+    todoEls.forEach((e) => {
+        todoArr.push({
+            text: e.innerText,
+            completed: e.classList.contains("completed")
+        });
+
+    localStorage.setItem("todos", JSON.stringify(todoArr))
+    });
+}
 
     
