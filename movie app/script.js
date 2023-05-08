0d9fd207b936aa6e4916df8394f50a6791e4fd7a
Tirthankar Nath <tirthankar.nath2@gmail.com>
2023-05-08 20:52:19 +0530

    the api is working fine and able to fetch image directly

--- movie app/script.js
+++ movie app/script.js
@@ -1,19 +1,25 @@
-const APIURL = "http://";
-const IMGPATH = "https://";
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-async function getMovies(){
-    const resp = await fetch(APIURL); //resp fetches data from the API URL
-    
-    const respData = await resp.json(); //all data from json of API is stored in respData variable
+const IMGPATH = "https://image.tmdb.org/t/p/w500/";
+//got this from the official themoviedb.org documentation
 
-    respData.results.forEach(m => {
+
+async function getMovies() {
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    respData.results.forEach(movie =>{
         const img = document.createElement("img");
-        img.src = IMGPATH + m.poster_path;
+//adding the poster path with the common source
+        img.src = IMGPATH + movie.poster_path;
 
         document.body.appendChild(img);
     });
-
     return respData;
 }
+//xtra: console.log(getMovies()); //returns a pending promise
 
 getMovies();
