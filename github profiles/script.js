b4f219b87b1eb904d1f612165a34558f74502ce8
Tirthankar Nath <tirthankar.nath2@gmail.com>
2023-05-11 17:13:02 +0530

    the api has been integrated successfully, need to improve the design of the interface

---
+++ github profiles/script.js
@@ -0,0 +1,75 @@
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+
+async function getUser(username) {
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+
+    createUserCard(respData);
+
+    // getRepos(username);
+}
+
+// if you had to add elements like you did in notes app
+// function createUserCard(user){
+//     const card = document.createElement('div');
+//     card.classList.add('card');
+
+//     card.innerHTML = `
+//     <div>
+//         <img src = "${user.avatar_url}" alt = "${user.name}">
+//     </div>
+//     <div>
+//         <h2>${user.name}</h2>
+//         <p>${user.bio}</p>
+
+//         <ul>
+//             <li>${user.followers}</li>
+//             <li>${user.following}</li>
+//             <li>${user.public_repos}</li>
+//         </ul>
+//     </div>
+//     `
+
+//     main.appendChild(card);
+// }
+
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul class="info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);   
+
+        search.value = "";
+    }
+});
